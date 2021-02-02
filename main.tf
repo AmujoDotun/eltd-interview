67e6809c0ce94a7b53660110ea7b6f96ad835251
AmujoDotun <samuelamujo@gmail.com>
2021-02-02 17:44:58 +0100

    removed iam resourse and policy

--- main.tf
+++ main.tf
@@ -136,67 +136,7 @@ resource "aws_instance" "web-server-instance" {
 
 
 
-  # create iam role
-
-resource "aws_iam_role" "test_role" {
-  name = "test_role"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-
-  tags = {
-    tag-key = "tag-value"
-  }
-}
-
- # creating instance profile to link iam role
-  resource "aws_iam_instance_profile" "test_profile" {
-  name = "test_profile"
-  role = "${aws_iam_role.test_role.name}"
-}
-
-
-# adding iam policy
-resource "aws_iam_role_policy" "test_policy" {
-  name = "test_policy"
-  role = "${aws_iam_role.test_role.id}"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "s3:*"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-}
-
-# Attaching role to Ec2 instance
-resource "aws_instance" "role-test" {
-  ami = "ami-0bbe6b35405ecebdb"
-  instance_type = "t2.micro"
-  iam_instance_profile = "${aws_iam_instance_profile.test_profile.name}"
-  key_name = "mytestpubkey"
-}
+ 
 
 
   user_data = <<-EOF
